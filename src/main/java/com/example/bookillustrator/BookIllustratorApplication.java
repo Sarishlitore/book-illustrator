e5de5d789beeaef748a1bbdf10d5471c510c08a4
sarish <rishat.sabirovgm@gmail.com>
2025-06-20 18:59:47 +0300

    Initialize project: Spring Boot 3.5.3, Java 17, Spring Web + Data JPA + H2

---
+++ src/main/java/com/example/bookillustrator/BookIllustratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookillustrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookIllustratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookIllustratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookillustrator/BookIllustratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookillustrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookIllustratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
